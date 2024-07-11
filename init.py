8a321e6193c27ab74360f071ac2f522d6f9ed625
Rehnkhan <rk1713335@gmail.com>
2024-07-11 16:25:01 +0530

    Add files via upload

---
+++ init.py
@@ -0,0 +1,29 @@
+import sys, getopt
+import scheduler 
+
+def main(argv):
+
+   # default algorithm:
+   workers = 3
+
+   try:
+      opts, args = getopt.getopt(argv,"w:",["workers="])
+   except getopt.GetoptError:
+      print("init.py -w <workers>")
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt in ("-w", "--workers"):
+         # use alternative workes value if provided:
+         workers = arg
+   print ("Using no of workers: ", workers)
+   
+   #calling the scheduler class constructor
+   s=scheduler.scheduler()
+   s.schedule("tasks")
+   
+   import coordinator
+   coordinator.Coordinator(int(workers))
+   
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
